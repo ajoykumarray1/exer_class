3f76a3175b99bf1c27edc0a7256176e5fb0b2c9e
ajoy123 <asad@gmail.com>
2023-09-14 13:30:59 +0600

    this is my class practice

---
+++ prob.py
@@ -0,0 +1,42 @@
+# a=int(input("First number:"))
+# b=int(input("Second number:"))
+# sum=a+b
+# difference=a-b
+# product=a*b
+# quotient=a/b
+# modulus=a%b
+# print("Sum =",sum)
+# print("Difference =",difference)
+# print("Product =",product)
+# print("Quotient =",int(quotient))
+# print("Modulus =",modulus)
+
+
+# a=int(input("Enter length:"))
+# b=int(input("Enter width:"))
+# peri=2*(a+b)
+# print("Perimeter of rectangle= {} units".format(peri))
+
+# a=int(input("Enter length:"))
+# b=int(input("Enter width:"))
+# ar=a*b
+# print("Area of rectangle= {} sq.units".format(ar))
+
+# r=int(input("Enter radius:"))
+# pi=3.1416
+# dia=2*r
+# cir=2*pi*r
+# ar=pi*r**2
+
+# print("Diameter= {} units".format(dia))
+# print("Circumference= {} units".format(cir))
+# print("Area= {} sq.units".format(int(ar)))
+
+
+# c=int(input("Enter lenth in cenimeter:"))
+# m=c/100
+# km=c/100000
+# print("Length in meter= {} m".format(int(m)))
+# print("Length in kilometer= {} km".format(km))
+
+
---
+++ sort.py
@@ -0,0 +1,10 @@
+l=[10,5,15,6,8,9]
+for i in range(len(l)-1):
+    for j in range(0, len(l) - i - 1):
+        if l[j]>l[j+1]:
+            tem=l[j]
+            l[j]=l[j+1]
+            l[j+1]=tem
+
+print("after ascending",l)
+        
